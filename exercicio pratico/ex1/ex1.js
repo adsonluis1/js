26b8ad40569140360d704d9c60ef9a4e5b4f2bff
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-04-26 12:40:22 -0300

    26/04

--- exercicio pratico/ex1/ex1.js
+++ exercicio pratico/ex1/ex1.js
@@ -3,19 +3,31 @@ function carregar(){
     var img = document.getElementById('foto')
     var horaatual = new Date()
     var hora = horaatual.getHours()
+
+    
+    
+    
+    
     
     if(hora >= 6 && hora <=11){
-        titulo.innerText=`são ${hora}, está de manhã`
-       
+        titulo.innerText=`são ${hora} horas, está de manhã`
+        img.src= 'manha-img.jpg'
+        document.body.style.background = '#f49e12'
     }
     else if(hora >= 12 && hora <=18){
-        titulo.innerText=`são ${hora}, está de tarde`
+        titulo.innerText=`são ${hora} horas, está de tarde`
+        img.src= 'tarde-img.jpg'
+        document.body.style.background= '#f4a16e'
     }
     else if(hora >= 19 && hora <=23){
-        titulo.innerText=`são ${hora}, está de noite`
+        titulo.innerText=`são ${hora} horas, está de noite`
+        img.src= 'noite-img.jpeg'
+        document.body.style.background= '#191970'
     }
     else{
-        titulo.innerText=`são ${hora}, está de madrugada`
+        titulo.innerText=`são ${hora} horas, está de madrugada`
+        img.src= 'noite-img.jpeg'
+        document.body.style.background= '#191970'
     }
 }
 

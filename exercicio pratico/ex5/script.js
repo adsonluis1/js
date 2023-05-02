ebe12051c8315526cec62bc9d37055277f64fac5
adsonluis1 <129302344+adsonluis1@users.noreply.github.com>
2023-05-02 17:17:51 -0300

    02/05

--- exercicio pratico/ex5/script.js
+++ exercicio pratico/ex5/script.js
@@ -1,24 +1,8 @@
-let numero = document.getElementById('numero')
-let tela = document.getElementById('tela')
-let tela2 = document.getElementById('tela2')
-let valores = []
-
-function isnumero(n){
-    if(Number(n) >= 1 || Number(n) <= 100){
-        return true
-    }else{
-        return false
-    }
-}
-
-function islist(n , l){
-
-}
-
 
 
 function adicionar(){
-    if(isnumero(numero.value) && islist(numero.value , valores)){
-    
-    }else{alert('hihi')}
+
+    let numero = document.querySelector('input#numero')
+let tela = document.querySelector('tela')
+    tela.innerHTML = `${numero}`
 }
